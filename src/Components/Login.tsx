7d0720f7fff1f0d422b22be047015e5354f89aae
Abdul Shakoor <ibupoto@gmail.com>
2026-06-24 02:50:25 +0500

    ......

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,32 +1,34 @@
-
+import { useState } from "react";
 
 const Login = () => {
-    const submithandler=(e)=>{ 
-        console.log("Form submitted")
-        e.preventDefault()
-    }
+  const [email, setEmail] = useState("");
+
+  const submithandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    console.log(email);
+  };
+
   return (
     <div>
-        <form onClick={(e)=>submithandler(e)}>
-                <input
-                value=""
-                className="border border-black-300"
-                type="text"
-                placeholder="Enter Email"
-                />
-                <input 
-                 className="border border-black-300"
-                type="password"
-                placeholder="Enter password"
-                />
-                <button
-                className="m-5 p-1 bg-gray-300 border border-black rounded"
-                type="submit">
-                Submit
-                </button>
-            </form>
-    </div>
-  )
-}
+      <form onSubmit={submithandler}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border border-black"
+          type="text"
+          placeholder="Enter Email"
+        />
 
-export default Login
+        <button
+          className="m-5 p-1 bg-gray-300 border border-black rounded"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
