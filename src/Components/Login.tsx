2cedc87a75e09bd269909474860160f399762122
Abdul Shakoor <ibupoto@gmail.com>
2026-06-24 13:16:04 +0500

    .....

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 
 const Login = () => {
 const [email, setEmail]= useState("");
+const [passWord, setpassWord]= useState("");
 
 
 
@@ -15,9 +16,17 @@ const [email, setEmail]= useState("");
         type="email"
         placeholder="Enter Email"
         value={email}
-        onChange={(e)=>setEmail(e.target.value)}
+        onChange={(e)=>setpassWord(e.target.value)}
         />
         <h2>{email}</h2>
+        <input 
+        className="border border-black"
+        type="passWord"
+        placeholder="Enter password"
+        value={passWord}
+        onChange={(e)=>setEmail(e.target.value)}
+        />
+        <h2>{passWord}</h2>
       </form>
     </div>
   )
