625c30f3eb0f2adbd17c7cc803e4136f450c2ef8
Abdul Shakoor <ibupoto@gmail.com>
2026-07-03 12:14:07 +0500

    ....

--- src/Components/basics/Deleteuser2.tsx
+++ src/Components/basics/Deleteuser2.tsx
@@ -1,40 +1,35 @@
 import { useState } from "react";
-
 const DeleteUser2 = () => {
-  const [users, setUsers] = useState([
-    { id: 1, name: "Anas", age: 10 },
-    { id: 2, name: "Onais", age: 15 },
-    { id: 3, name: "Ali", age: 20 },
-  ]);
+    const [users, setUsers] = useState([
+        { id: 1, name: "Anas", age: 10 },
+        { id: 2, name: "Onais", age: 15 },
+        { id: 3, name: "Ali", age: 20 },
+    ]);
+    const deleteUser = (id: number) => {
+        const updatedUsers = users.filter((user) => user.id !== id);
+        setUsers(updatedUsers);
+    };
+    return (
+        <div>
+            <h1>User List</h1>
+            <p>Total Users: {users.length}</p>
+            {users.length === 0 ? (
+                <h2>No Users Available</h2>
+            ) : (
+                users.map((user) => (
+                    <div key={user.id}>
+                        <h2>{user.name}</h2>
 
-  const deleteUser = (id: number) => {
-    const updatedUsers = users.filter((user) => user.id !== id);
-    setUsers(updatedUsers);
-  };
+                        <p>Age: {user.age}</p>
 
-  return (
-    <div>
-      <h1>User List</h1>
-
-      <p>Total Users: {users.length}</p>
-
-      {users.length === 0 ? (
-        <h2>No Users Available</h2>
-      ) : (
-        users.map((user) => (
-          <div key={user.id}>
-            <h2>{user.name}</h2>
-
-            <p>Age: {user.age}</p>
-
-            <button onClick={() => deleteUser(user.id)}>
-              Delete
-            </button>
-          </div>
-        ))
-      )}
-    </div>
-  );
+                        <button onClick={() => deleteUser(user.id)}>
+                            Delete
+                        </button>
+                    </div>
+                ))
+            )}
+        </div>
+    );
 };
 
 export default DeleteUser2;
