ad898020678095b233aa41ce8ddf077b107a47e5
Abdul Shakoor <ibupoto@gmail.com>
2026-07-03 12:55:19 +0500

    .......

--- src/Components/basics/Deleteuser2.tsx
+++ src/Components/basics/Deleteuser2.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 
 
 const Deleteuser2 = () => {
+    const [users, setusers] = useState([
+        {id:1, namae:"Mehran", age: 10, city:"Padidan"},
+        {id:2, namae:"Anas", age: 20, city:"Padidan"},
+        {id:3, namae:"Onais", age: 5, city:"Padidan"},
+        {id:4, namae:"Sagar", age: 9, city:"Padidan"},
+    ]);
   return (
     <div>
       
