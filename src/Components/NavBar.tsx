263356e391a2b0fd24ebf1e1a7023a7c648a7de1
Abdul Shakoor <ibupoto@gmail.com>
2026-07-05 14:03:19 +0500

    ....

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,7 +6,14 @@ const NavBar = () => {
         <header className="w-full shadow-md sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
-
+                  <nav>
+                    <ul>Home</ul>
+                    <ul>Gallery</ul>
+                    <ul>Product</ul>
+                    <ul></ul>
+                    <ul></ul>
+                    <li></li>
+                  </nav>
                 </div>
             </div>
         </header>
