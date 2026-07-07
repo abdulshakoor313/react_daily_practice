bb8092506e435ff6a95b5aa567cbe4e36dc72074
Abdul Shakoor <ibupoto@gmail.com>
2026-07-07 12:42:17 +0500

    ......

--- src/Components/forms/AddUser.tsx
+++ src/Components/forms/AddUser.tsx
@@ -1,21 +1,45 @@
 import {useState} from 'react'
 
 const AddUser = () => {
-    const [addUser, setAdduser] = useState([
+    const [name, setName] = useState("")
+    const [users, setUsers] = useState([
         {id:1, name:"Mehran", age: 10, city:"Padidan"},
         {id:2, name:"Anas", age: 20, city:"Padidan"},
         {id:3, name:"Onais", age: 5, city:"Padidan"},
         {id:4, name:"Sagar", age: 9, city:"Padidan"},
-    ])
+    ]);
+    const addUser = () => {
+    const newUser = {
+      id: users.length + 1,
+      name: name,
+      age: 18,
+    };
+    setUsers([...users, newUser]);
+
+    setName("");
+  };
+
   return (
     <div>
       <input 
 
       className='border-2'
       type='text'
+      value={name}
       placeholder='Name '
+       onChange={(e) => setName(e.target.value)}
       />
-      Add the User
+     <button
+     onClick={addUser}
+     >
+        Add
+        </button>
+          {users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.age}</p>
+        </div>
+      ))}
     </div>
   )
 }
