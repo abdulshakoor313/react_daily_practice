b00d473b2c3ad87bdb98c6bf02c4fd4f10ced5dc
Abdul Shakoor <ibupoto@gmail.com>
2026-07-10 13:20:10 +0500

    ...

--- src/Components/basics/UssseEffect.tsx
+++ src/Components/basics/UssseEffect.tsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react"
 
 const UssseEffect = () => {
-  const [Count, setCount]=useState(0);
-  useEffect(()=>{
+  const [Count, setCount] = useState(0);
+  useEffect(() => {
     console.log("Changed the value");
-  },[Count]);
+  }, [Count]);
   return (
     <div>
       <h1>{Count}</h1>
-      <button onClick={()=>setCount(Count+1)}>Increase</button>
-      <button onClick={()=>setCount(Count-1)}>Decrease</button>
-      <button onClick={()=>setCount(0)}>Reset</button>
+      <button onClick={() => setCount(Count + 1)}>Increase</button>
+      <button onClick={() => setCount(Count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
