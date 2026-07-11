5a75fc0a0c31f9bb9e2e4e228c97ce27676e1a72
Abdul Shakoor <ibupoto@gmail.com>
2026-07-11 21:11:27 +0500

    ......

--- src/Components/forms/AddUser.tsx
+++ src/Components/forms/AddUser.tsx
@@ -1,42 +1,126 @@
-import { useState } from 'react'
+import { useState } from "react"
+
 
 const AddUser = () => {
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
   const [users, setUsers] = useState([
     { id: 1, name: "Mehran", age: 10, },
     { id: 2, name: "Sagar", age: 9, },
   ]);
-  const addUser = () => {
-    const newUser = {
-      id: users.length + 1,
-      name: name,
-      age: 18,
-    };
-    setUsers([...users, newUser]);
-    setName("");
-  };
+  const AddUser = () =>{
+    const newUser = {id, name, age}
+  }
   return (
     <div>
-      <input
-        className='border-2'
-        type='text'
-        value={name}
-        placeholder='Name '
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button
-        onClick={addUser}
-      >
-        Add
-      </button>
-      {users.map((user) => (
-        <div key={user.id}>
-          <h2>{user.name}</h2>
-          <p>{user.age}</p>
-        </div>
-      ))}
+      
     </div>
   )
 }
 
 export default AddUser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+
+// const AddUser = () => {
+//   const [name, setName] = useState("")
+//   const [users, setUsers] = useState([
+//     { id: 1, name: "Mehran", age: 10, },
+//     { id: 2, name: "Sagar", age: 9, },
+//   ]);
+//   const addUser = () => {
+//     const newUser = {
+//       id: users.length + 1,
+//       name: name,
+//       age: 18,
+//     };
+//     setUsers([...users, newUser]);
+//     setName("");
+//   };
+//   return (
+//     <div>
+//       <input
+//         className='border-2'
+//         type='text'
+//         value={name}
+//         placeholder='Name '
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <button
+//         onClick={addUser}
+//       >
+//         Add
+//       </button>
+//       {users.map((user) => (
+//         <div key={user.id}>
+//           <h2>{user.name}</h2>
+//           <p>{user.age}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default AddUser
