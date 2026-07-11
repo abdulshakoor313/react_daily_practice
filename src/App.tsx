7311bddb7fec65bf55065b4b6d8e8160747c8b7f
Abdul Shakoor <ibupoto@gmail.com>
2026-07-11 20:18:05 +0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Gallery from "./pages/Gallery";
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Deleteuser2 from "./Components/basics/Deleteuser2";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
+      <Deleteuser2/>
     </>
   );
 }
