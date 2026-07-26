2bd88595392e90f8ba728550ad5f154875c551b4
Abdul Shakoor <ibupoto@gmail.com>
2026-07-26 23:46:23 +0500

    ......

--- src/Components/forms/AddUser.tsx
+++ src/Components/forms/AddUser.tsx
@@ -23,9 +23,12 @@ const AddUser = () => {
       value={name}
       onChange={(e)=>{setName(e.target.value);
       }}
-
-      
       />
+      <button 
+      onClick={addUser}
+      >
+        Add
+      </button>
     </div>
   )
 }
